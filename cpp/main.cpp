2be47d05410bf3aea5ae32212f3ed54a7c324c69
Sean <seanwaweru551@gmail.com>
2026-06-01 21:41:56 +0300

    First touches

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
+    int guess;
     cout << "This is where we build!!" << endl;
+    cout << "I want to build a number guessing game like the one I enjoyed with Nicole." << endl;
+    
+    cout << "Guess a four digit number please: " << endl;
+    cin >> guess;
+
+    cout << "You guessed: " << guess << endl;
     return 0;
 }
